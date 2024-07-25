16ebcfa47fe1cb525f1e3aa936a4b6b419990874
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-25 11:51:54 +0200

    "Removed Footer import and component from App.svelte, added a layout file with skeleton code."

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script>
     import Header from './components/Header.svelte';
-    import Footer from './components/Footer.svelte'
 </script>
 
 <Header />
 <!-- Your content here -->
-<Footer />
+
---
+++ src/components/layout.svelte
@@ -0,0 +1,21 @@
+<!-- src/components/Layout.svelte -->
+<script>
+    // Layout logic
+  </script>
+  
+  <style>
+    /* Layout styles */
+  </style>
+  
+  <header>
+    <!-- Header content -->
+  </header>
+  
+  <main>
+    <slot></slot>
+  </main>
+  
+  <footer>
+    <!-- Footer content -->
+  </footer>
+  
