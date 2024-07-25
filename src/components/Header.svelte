b18cd7884a0d0bd8f4ce1caab711db49a3936aee
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-25 10:41:07 +0200

    "Uploaded and updated image source path in Header component"

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -17,7 +17,7 @@
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/">
           <button class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/online-shop.png" class="h-8" alt="SwiftCart Logo" />
+            <img src="./assets/online-shop.png" class="h-8" alt="SwiftCart Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
               SwiftCart
             </span>
