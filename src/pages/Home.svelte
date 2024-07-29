1337ebcc716d256d51cb305c1bc6fbe0da7eb259
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-29 14:18:37 +0200

    "Imported and added filter class in the Home page "

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,10 +1,12 @@
 <script>
+  import Filter from "../components/Filter.svelte";
   import Header from "../components/Header.svelte";
   import Sort from "../components/Sort.svelte";
 </script>
 
 
-<Header/>
+<Header />
+<Filter />
 <Sort />
 <h1>Welcome to Home Page</h1>
 
