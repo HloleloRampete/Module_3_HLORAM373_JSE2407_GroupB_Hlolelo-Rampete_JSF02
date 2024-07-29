07480895c63c5213f0cc1d1d166485f152fa2a33
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-29 13:09:27 +0200

    "Imported Header component into Home page"

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,11 +1,13 @@
 <!-- src/pages/Home.svelte -->
 <script>
   // Home page logic
+  import Header from "../components/Header.svelte";
 </script>
 
 <style>
   /* Home page styles */
 </style>
 
+<Header/>
 <h1>Welcome to Home Page</h1>
 
